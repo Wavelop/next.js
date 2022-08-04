990a41483482d85e08612b2d2a3afe6d24f54478
Matteo Granzotto <tools@matteogranzotto.com>
2022-08-04 17:16:21 +0200

    build(workaround): crated a branch only for build

--- packages/react-dev-overlay/dist/internal/components/ShadowPortal.d.ts
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-export declare type ShadowPortalProps = {
-    children: React.ReactNode;
-    globalOverlay?: boolean;
-};
-export declare const ShadowPortal: React.FC<ShadowPortalProps>;
